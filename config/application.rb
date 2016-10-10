3a2df82a73c3394aacce5caa5d664736ac13198c
Alfred Lotho <alfred.lotho@gmail.com>
2016-10-10 21:08:21 +0900

    add mongoid configuration to connect to the MongoDB database server

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module ItineraryWebservices
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      
+    #bootstraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
   end
 end
